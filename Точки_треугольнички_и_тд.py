fab4fc411654545eee9ba67eb0d6e01570a70ab5
egprivalov <44251572+egprivalov@users.noreply.github.com>
2021-10-04 20:24:02 +0500

    Update Точки_треугольнички_и_тд.py

--- Точки_треугольнички_и_тд.py
+++ Точки_треугольнички_и_тд.py
@@ -4,6 +4,17 @@ from random import randint
 def toFixed(numObj, digits=0):
     return f"{numObj:.{digits}f}"
 
+class Figure():
+    def __init__(self):
+        self.plosh=self.ploshad()
+        self.perim=self.perimetr()
+    def perimetr(self):
+        pass
+    def ploshad(self):
+        pass
+    def ShowClass(self):
+        return("Фигура")
+
 class Point():
     def __init__(self, xp,yp):
         self.x,self.y=xp,yp
@@ -14,135 +25,175 @@ class Point():
         print("("+str(self.x)+", "+str(self.y)+")")
         pass
 
-class Triangle():
+class Triangle(Figure):
     def __init__(self, a, b, c):
-        self.side1=float(toFixed(sqrt((b.x-a.x)**2+(b.y-a.y)**2), accuracy))
-        self.side2=float(toFixed(sqrt((c.x-b.x)**2+(c.y-b.y)**2), accuracy))
-        self.side3=float(toFixed(sqrt((a.x-c.x)**2+(a.y-c.y)**2), accuracy))
-        self.corner1=a
-        self.corner2=b
-        self.corner3=c
+        self.sides=[float(toFixed(sqrt((b.x-a.x)**2+(b.y-a.y)**2), accuracy)),
+                    float(toFixed(sqrt((c.x-b.x)**2+(c.y-b.y)**2), accuracy)),
+                    float(toFixed(sqrt((a.x-c.x)**2+(a.y-c.y)**2), accuracy))]
+        self.corners=[a,b,c]
+        self.plosh = self.ploshad()
+        self.perim = self.perimetr()
     def perimetr(self):
-        return(float(toFixed(self.side1+self.side2+self.side3, accuracy)))
+        return(float(toFixed(sum(self.sides), accuracy)))
     def ploshad(self):
         p=self.perimetr()/2
-        return(float(toFixed(sqrt(p*(p-self.side1)*(p-self.side2)*(p-self.side3)),accuracy)))
+        return(float(toFixed(sqrt(p*(p-self.sides[0])*(p-self.sides[1])*(p-self.sides[2])),accuracy)))
+    def ShowClass(self):
+        return("Треугольник")
 
-def CreatePoint(A):
-    x,y=map(float, input("Введите координаты новой точки: ").split())
-    A.append(Point(x,y))
-    print("Точка создана")
-    return(A[-1])
+class Rectangle(Figure):
+    def __init__(self, a, b, c, d):
+        self.corners=[a,b,c,d]
+        self.sides=[float(toFixed(sqrt((b.x-a.x)**2+(b.y-a.y)**2), accuracy)),
+                    float(toFixed(sqrt((c.x-b.x)**2+(c.y-b.y)**2), accuracy)),
+                    float(toFixed(sqrt((d.x-c.x)**2+(d.y-c.y)**2), accuracy)),
+                    float(toFixed(sqrt((d.x-a.x)**2+(d.y-a.y)**2), accuracy))]
+        self.plosh=self.ploshad()
+        self.perim=self.perimetr()
+    def perimetr(self):
+        return (float(toFixed(sum(self.sides), accuracy)))
+    def ploshad(self):
+        return (float(toFixed(self.sides[0]*self.sides[1],accuracy)))
+    def ShowClass(self):
+        return("Прямоугольник")
+
+class Circle(Figure):
+    def __init__(self,centr, R):
+        self.centr=centr
+        self.radius=R
+        self.plosh=self.ploshad()
+        self.perim=self.perimetr()
+    def perimetr(self):
+        return(float(toFixed(2*pi*self.radius,accuracy)))
+    def ploshad(self):
+        return(float(toFixed(pi*self.radius**2,accuracy)))
+    def ShowClass(self):
+        return("Круг")
 
 def CreateTriangle(A, a_point, b_point, c_point):
     A.append(Triangle(a_point, b_point, c_point))
     print("Треугольник создан")
     return(A[-1])
 
-def CreateRandomPoint(A):
-    A.append(Point(randint(-100, 100), randint(-100, 100)))
-    print("Точка создана")
-    return (A[-1])
-
 def CreateRandomTriangle(A):
     A.append(Triangle(Point(randint(-100, 100), randint(-100, 100)), Point(randint(-100, 100), randint(-100, 100)), Point(randint(-100, 100), randint(-100, 100))))
     print("Треугольник создан")
     return (A[-1])
 
-points=[]
-triangles=[]
+def CreateRectangle(A,a_point, b_point, c_point, d_point):
+    A.append(Rectangle(a_point, b_point, c_point, d_point))
+    print("Прямоугольник создан")
+    return (A[-1])
+
+def CreateRandomRectangle(A):
+    a_point=Point(randint(-100, 100), randint(-100, 100))
+    angle=randint(0,90)
+    side_1=randint(0,100)
+    side_2=randint(0,100)
+    b_point=Point(float(toFixed(a_point.x+side_1*cos(angle),accuracy)),float(toFixed(a_point.y+side_1*sin(angle),accuracy)))
+    c_point=Point(float(toFixed(b_point.x+side_2*cos(90-angle), accuracy)),float(toFixed(b_point.y-side_2*sin(90-angle),accuracy)))
+    d_point=Point(float(toFixed(a_point.x-b_point.x+c_point.x,accuracy)), float(toFixed(a_point.y+c_point.y-b_point.y,accuracy)))
+    A.append(Rectangle(a_point, b_point, c_point, d_point))
+    print("Прямоугольник создан")
+    return (A[-1])
+
+def CreateCircle(A, centr, R):
+    A.append(Circle(centr,R))
+    print("Круг создан")
+    return(A[-1])
+
+def CreateRandomCircle(A):
+    A.append(Circle(Point(randint(-100, 100), randint(-100,100)),randint(0,100)))
+    print("Круг создан")
+    return(A[-1])
+
+figures=[]
 accuracy=2
 
 if __name__ == '__main__':
     k=1
     while k:
         k = int(input("\nВыберите действие:\n"
-                      "Для создания новой точки-введите 1\n"
-                      "Для вывода координат точек-введите 2\n"
-                      "Для изменения координат точки-введите 3\n"
-                      "Для создания нового треугольника-введите 4\n"
-                      "Для вывода списка треугольников-введите 5\n"
-                      "Для подсчета периметра треугольника-введите 6\n"
-                      "Для подсчета площади треугольника-введите 7\n"
-                      "Для демонстрации работы программы-введите 8\n"
+                      "Для создания треугольника-введите 1\n"
+                      "Для создания прямоугольника-введите 2\n"
+                      "Для создания круга-введите 3\n"
+                      "Для вывода списка фигур-введите 4\n"
+                      "Для вывода площади и периметра фигуры-введите 5\n"
+                      "Для сортировки массива по периметру-введите 6\n"
+                      "Для сортировки массива по площади-введите 7\n"
                       "Для изменения точности измерения-введите 9\n"
                       "Для выхода-введите 0\n"))
 
         if k==1:
-            key=int(input("\nДля создания точки по заданным координатам-введите 1\n"
-                          "Для создания случайной точки-введите 2\n"))
-            if key-1:
-                CreateRandomPoint(points)
-            else:
-                CreatePoint(points)
-
-        elif k==2:
-            for i in range(len(points)):
-                print(str(i+1)+"-я точка: ", end="")
-                points[i].vyvod()
-
-        elif k==3:
-            number=int(input("\nВведите номер точки, которую хотите изменить: "))-1
-            xnew,ynew=map(float, input("Введите новые координаты: ").split())
-            points[number].change(xnew, ynew)
-            print("Изменения внесены")
-
-        elif k==4:
             key = int(input("\nДля создания треугольника по заданным координатам-введите 1\n"
                             "Для создания случайного треугольника-введите 2\n"))
             if key - 1:
-                CreateRandomTriangle(triangles)
+                CreateRandomTriangle(figures)
             else:
                 x1,y1=map(float, input("Введите координаты первой точки: ").split())
                 x2,y2=map(float, input("Введите координаты второй точки: ").split())
                 x3,y3=map(float, input("Введите координаты третьей точки: ").split())
-                CreateTriangle(triangles, Point(x1,y1), Point(x2,y2), Point(x3,y3))
+                CreateTriangle(figures, Point(x1,y1), Point(x2,y2), Point(x3,y3))
+
+        elif k==2:
+            key = int(input("\nДля создания прямоугольника по заданным координатам-введите 1\n"
+                            "Для создания случайного пряумоугольника-введите 2\n"))
+            if key - 1:
+                CreateRandomRectangle(figures)
+            else:
+                x1,y1=map(float, input("Введите координаты первой точки: ").split())
+                x2,y2=map(float, input("Введите координаты второй точки: ").split())
+                x3,y3=map(float, input("Введите координаты третьей точки: ").split())
+                x4, y4 = map(float, input("Введите координаты четвертой точки: ").split())
+                CreateRectangle(figures, Point(x1,y1), Point(x2,y2), Point(x3,y3), Point(x4,y4))
+
+        elif k==3:
+            key = int(input("\nДля создания круга по заданным координатам-введите 1\n"
+                            "Для создания случайного круга-введите 2\n"))
+            if key - 1:
+                CreateRandomCircle(figures)
+            else:
+                x,y=map(float,input("Введите координаты центра: ").split())
+                R=float(input("Введите радиус: "))
+                CreateCircle(figures,Point(x,y),R)
+
+        elif k==4:
+            for i in range(len(figures)):
+                print(f"{i+1}-я фигура:\n    {figures[i].ShowClass()}")
+                if figures[i].ShowClass()=="Треугольник":
+                    for j in range(3):
+                        print(f"{j+1}-я точка: ({figures[i].corners[j].x}, {figures[i].corners[j].y})")
+                elif figures[i].ShowClass()=="Прямоугольник":
+                    for j in range(4):
+                        print(f"{j+1}-я точка: {figures[i].corners[j].x,figures[i].corners[j].y}")
+                elif figures[i].ShowClass()=="Круг":
+                    print(f"Центр: ({figures[i].centr.x}, {figures[i].centr.y})")
+                    print(f"Радиус: {figures[i].radius}")
+            if not len(figures):
+                print("Нет фигур :(")
 
         elif k==5:
-            for i in range(len(triangles)):
-                print(str(i+1)+"-й треугольник:")
-                print("Стороны: ", end="")
-                print(triangles[i].side1, triangles[i].side2, triangles[i].side3)
-                print("Координаты углов: ")
-                triangles[i].corner1.vyvod()
-                triangles[i].corner2.vyvod()
-                triangles[i].corner3.vyvod()
-                print()
+            number=int(input("Введите номер фигуры, периметр которой хотите узнать: ")) - 1
+            print("Периметр выбранной фигуры равен:", figures[number].perimetr())
+            print("Площадь выбранной фигуры равна:", figures[number].ploshad())
 
         elif k==6:
-            number=int(input("Введите номер треугольника, периметр которого хотите узнать: ")) - 1
-            print("Периметр выбранного треугольника равен:", triangles[number].perimetr())
+            key=int(input("\nЧтобы отсортировать по убыванию-введите 1\n"
+                    "Чтобы отсортировать по возрастанию-введите 2\n"))-1
+            if not key:
+                figures=sorted(figures, key=lambda x: x.perimetr(), reverse=True)
+            else:
+                figures = sorted(figures, key=lambda x: x.perimetr())
+            print("Фигуры отсортированы")
 
         elif k==7:
-            number=int(input("Введите номер треугольника, площадь которого хотите узнать: ")) - 1
-            print("Площадь выбранного треугольника равна:", triangles[number].ploshad())
-
-        elif k==8:
-            for i in range(2):
-                CreateRandomTriangle(triangles)
-            print()
-            for i in range(2):
-                print("Треугольник", str(i+1)+'-ый:')
-                print("Стороны:")
-                print(triangles[len(triangles)-1-i].side1)
-                print(triangles[len(triangles)-1-i].side2)
-                print(triangles[len(triangles)-1-i].side3)
-                print()
-                print("Координаты вершин:")
-                triangles[len(triangles)-1-i].corner1.vyvod()
-                triangles[len(triangles)-1-i].corner2.vyvod()
-                triangles[len(triangles)-1-i].corner3.vyvod()
-                print()
-                print("Периметр:")
-                print(triangles[len(triangles)-1-i].perimetr())
-                print("Площадь:")
-                print(triangles[len(triangles)-1-i].ploshad())
-                print()
-            for i in range(2):
-                CreateRandomPoint(points)
-            for i in range(2):
-                print(str(i + 1) + "-я точка: ", end="")
-                points[len(points)-1-i].vyvod()
+            key = int(input("\nЧтобы отсортировать по убыванию-введите 1\n"
+                            "Чтобы отсортировать по возрастанию-введите 2\n")) - 1
+            if not key:
+                figures = sorted(figures, key=lambda x: x.ploshad(), reverse=True)
+            else:
+                figures = sorted(figures, key=lambda x: x.ploshad())
+            print("Фигуры отсортированы")
 
         elif k==9:
             print("Знаки после запятой:", accuracy)
